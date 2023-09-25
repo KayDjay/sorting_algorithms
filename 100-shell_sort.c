6b04457617d5817a9d14acf8aa88432d260d5859
root <Khadijatrasaq@gmail.com>
2023-09-25 09:28:25 -0700

    Shell sort

---
+++ 100-shell_sort.c
@@ -0,0 +1,42 @@
+#include "sort.h"
+
+/**
+ * shell_sort - Function that sorts an array of integers
+ *		using Shell sort algorithm
+ *
+ * @array: The array to be sorted
+ * @size: Number of elements in the array
+ */
+i
+void shell_sort(int *array, size_t size)
+{
+	int gap = 1, a;
+	int temp, b;
+
+	if (array == NULL || size < 2)
+		return;
+
+	while (gap < (int)size / 3)
+		gap = gap * 3 + 1;
+
+	while (gap > 0)
+	{
+		for (a = gap; a < (int)size; a++)
+		{
+			temp = array[a];
+			b = a;
+
+			while (b >= gap && array[b - gap] > temp)
+			{
+				array[b] = array[b - gap];
+				b -= gap;
+			}
+
+				array[b] = temp;
+		}
+
+		print_array(array, size);
+
+		gap = (gap - 1) / 3;
+	}
+}
